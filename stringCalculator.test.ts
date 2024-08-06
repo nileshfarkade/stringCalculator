e4963bc24fc8133e92666df10a9dffbcb86ecf08
nileshfarkade <farkade.nilesh@gmail.com>
2024-08-06 15:03:31 +0530

    Update stringCalculator.test.ts
    Started writing test cases

--- stringCalculator.test.ts
+++ stringCalculator.test.ts
@@ -1,5 +1,7 @@
 import { add } from './stringCalculator.ts';
 
 describe('String Calculator', () => {
-  
+  test('Should return 0 for an empty string', () =>{
+    except(add("")).toBe();
+  });
 });
